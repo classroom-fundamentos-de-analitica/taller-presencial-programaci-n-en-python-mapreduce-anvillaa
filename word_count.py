b5c7b7add6045cd626142c4774f8f7106b523bab
anvillaa <111092507+anvillaa@users.noreply.github.com>
2024-02-12 11:25:58 -0500

    Update word_count.py

--- word_count.py
+++ word_count.py
@@ -12,9 +12,23 @@
 #     ...
 #     ('text2.txt'. 'hypotheses.')
 #   ]
-#
+# 
+#Funcion de procesamiento. Retornar una tupla la linea de archivo y la linea fisica.
+import glob
+import fileinput
+
 def load_input(input_directory):
-    pass
+
+    """Carga los archivos y generación de tupla """
+    filesnames=glob.glob(input_directory + "/*.*")
+
+    sequence = []   
+    with fileinput.input(files=filesnames) as f:
+        for line in f:
+            sequence.append(
+                (f.filename(), line )
+            )    
+    return sequence
 
 
 #
@@ -30,7 +44,14 @@ def load_input(input_directory):
 #   ]
 #
 def mapper(sequence):
-    pass
+
+    new_sequence = [
+        (word.lower().replace(".","").replace(",",""),1)
+        for _, line in sequence 
+        for word in line.split()
+    ]
+    return new_sequence
+
 
 
 #
@@ -45,7 +66,11 @@ def mapper(sequence):
 #   ]
 #
 def shuffle_and_sort(sequence):
-    pass
+    sequence=sorted(
+        sequence,
+        key = lambda x:x[0]
+    )
+    return sequence
 
 
 #
@@ -54,17 +79,32 @@ def shuffle_and_sort(sequence):
 # ejemplo, la reducción indica cuantas veces aparece la palabra analytics en el
 # texto.
 #
+#group by es muy eficiente para procesarlas 
+
+from itertools import groupby
+
 def reducer(sequence):
-    pass
+    new_sequence = []
+    for k, g in groupby(sequence, lambda x: x[0]):
+        key = k
+        value = sum(x[1] for x in g)
+        new_sequence.append(
+            (key,value)
+        )
+    return new_sequence
+   
 
 
 #
 # Escriba la función create_ouptput_directory que recibe un nombre de directorio
 # y lo crea. Si el directorio existe, la función falla.
-#
-def create_ouptput_directory(output_directory):
-    pass
+#buscar si el directorio existe y crear uno si efectivamente no existe.
 
+import os.path
+def create_ouptput_directory(output_directory):
+    if os.path.isdir(output_directory):
+        raise Exception("El directorio ya existe")
+    os.mkdir(output_directory)
 
 #
 # Escriba la función save_output, la cual almacena en un archivo de texto llamado
@@ -75,22 +115,34 @@ def create_ouptput_directory(output_directory):
 # separados por un tabulador.
 #
 def save_output(output_directory, sequence):
-    pass
+    #concatenar con os el nombre del directorio y del archivo
+    filename = os.path.join(output_directory, "part-00000")
+    with open(filename, "w") as f:
+        for key, Value in sequence:
+            f.write(f"{key}\t {Value}\n")
 
 
-#
 # La siguiente función crea un archivo llamado _SUCCESS en el directorio
 # entregado como parámetro.
 #
 def create_marker(output_directory):
-    pass
+    with open(os.path.join(output_directory, "SUCCESS"),"w") as f:
+        f.write("")
+
 
 
 #
 # Escriba la función job, la cual orquesta las funciones anteriores.
 #
 def job(input_directory, output_directory):
-    pass
+    sequence = load_input(input_directory)
+    sequence = mapper(sequence)
+    sequence = shuffle_and_sort(sequence)
+    sequence = reducer(sequence)
+    create_ouptput_directory(output_directory)
+    save_output(output_directory, sequence)
+    create_marker(output_directory)
+
 
 
 if __name__ == "__main__":
